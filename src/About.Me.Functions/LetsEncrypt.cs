f31d8db0261103a990f9c7d5c77536d7be7ee70d
Fernando Seguim <fernando.seguim@gmail.com>
2019-03-16 00:51:04 -0300

    add letsencrypt function as workarround to use letsencrypt ssl certificate

---
+++ src/About.Me.Functions/LetsEncrypt.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace About.Me.Functions
+{
+    public static class LetsEncrypt
+    {
+        [FunctionName("LetsEncrypt")]
+        public static Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "LetsEncrypt/.well-known/acme-challenge/{code}")]HttpRequest req, string code, TraceWriter log)
+        {
+            log.Info($"C# HTTP trigger function processed a request. {code}");
+
+            var content = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), @".well-known\acme-challenge\", code));
+            var resp = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(content, Encoding.UTF8, "text/plain")
+            };
+            return Task.FromResult(resp);
+        }
+    }
+}
