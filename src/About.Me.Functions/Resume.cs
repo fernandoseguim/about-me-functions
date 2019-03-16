731c5543ac939389d86c251380047a45dacc311d
Fernando Seguim <fernando.seguim@gmail.com>
2019-03-15 22:38:09 -0300

    rename function to resume

--- src/About.Me.Functions/Resume.cs
+++ src/About.Me.Functions/Resume.cs
@@ -9,9 +9,9 @@ using Newtonsoft.Json;
 
 namespace About.Me.Functions
 {
-    public static class Function1
+    public static class Resume
     {
-        [FunctionName("Function1")]
+        [FunctionName("resume")]
         public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
         {
             log.Info("C# HTTP trigger function processed a request.");
